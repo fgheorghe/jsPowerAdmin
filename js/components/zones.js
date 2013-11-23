79663d7d8dfd7f6477b32873ff019eed1a87e2a9
Grosan Flaviu Gheorghe <grosan.flaviu@yahoo.com>
2013-11-23 23:21:06 +0000

    Added stub zones grid.

--- js/components/zones.js
+++ js/components/zones.js
@@ -13,8 +13,57 @@ var zones = function() {
  * @function
  */
 zones.prototype.init = function() {
+        // Prepare view grid
+        this.createZoneGridPanel();
+
         // Create main panel
         this.createPanel();
+}
+
+/**
+ * Method used for creating the zone grid panel.
+ * @function
+ * @return {Object} Ext.grid.Panel object.
+ */
+zones.prototype.createZoneGridPanel = function() {
+        // Create store
+        // TODO: Add remote JSON store.
+        // NOTE: Stub.
+        this.zoneStore = Ext.create( 'Ext.data.Store', {
+                fields: [ 'name', 'type', 'records' ]
+                ,data: {
+                        items: [
+                               {
+                                        'name': 'test.com'
+                                        ,'type': 'master'
+                                        ,'records': 1
+                               }
+                        ]
+                }
+                ,proxy: {
+                       type: 'memory'
+                       ,reader: {
+                                type: 'json'
+                                ,root: 'items'
+                        }
+                }
+        } );
+
+        // Create grid panel
+        // TODO: Add remote sorting.
+        this.zoneGridPanel = Ext.create('Ext.grid.Panel', {
+                store: this.zoneStore
+                ,columns: [
+                        { text: 'Name',  dataIndex: 'name', flex: 1 }
+                        ,{ text: 'Type', dataIndex: 'type' }
+                        ,{ text: 'Records', dataIndex: 'records' }
+                        // TODO: Add owner column.
+                ]
+                ,height: 200
+                ,width: 400
+        });
+
+        return this.zoneGridPanel;
 }
 
 /**
@@ -26,6 +75,8 @@ zones.prototype.createPanel = function() {
         // Create main panel
         this.panel = Ext.create( 'Ext.Panel', {
                 title: 'Zones'
+                ,layout: 'fit'
+                ,items: this.zoneGridPanel
         } );
 
         return this.panel;
