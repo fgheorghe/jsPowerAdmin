30ab9a4de80b7ab3257421ef71c17084c3fafd9c
Grosan Flaviu Gheorghe <grosan.flaviu@yahoo.com>
2013-12-14 23:38:47 +0000

    Load ExtJS from a CDN, if production, otherwise use local debug version.

--- src/jsPowerAdmin/WebBundle/Controller/DefaultController.php
+++ src/jsPowerAdmin/WebBundle/Controller/DefaultController.php
@@ -12,8 +12,14 @@ class DefaultController extends Controller
         // Prepare UI specific configuration
         $configuration = $this->container->getParameter('js_power_admin_web.ui');
 
+        // Pass on the isDebug kernel value
+        // If enabled, ExtJS will be loaded via a CDN.
+        // See: services.yml for CDN path configuration.
+        $isDebug = $this->get( 'kernel' )->isDebug();
+
         return $this->render('jsPowerAdminWebBundle:Default:index.html.twig', array(
                 "configuration" => $configuration
+                ,"isDebug" => $isDebug
         ) );
     }
 }
